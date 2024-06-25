ce995ca72557c984a95ef43c23d86168ad3ec82d
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-25 16:15:18 -0300

    Breadcrumbs Navigation

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
+    <x-bread-crumbs class="mb-4"
+                    :links="['Jobs' => route('jobs.index')]"
+    ></x-bread-crumbs>
     @foreach($jobs as $job)
         <x-job-card :job="$job">
-
             <div>
                 <x-link-button :href="route('jobs.show', $job)">
                     Show
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
-   <x-job-card :job="$job">
 
-   </x-job-card>
+    <x-bread-crumbs class="mb-4"
+                    :links="['Jobs' => route('jobs.index'), $job->title => '#']"
+    ></x-bread-crumbs>
+
+    <x-job-card :job="$job">
+
+    </x-job-card>
 </x-layout>
