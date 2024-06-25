85af2ab610cdc4e66a1340192803d83297c30240
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-25 15:49:16 -0300

    Job Page: Job Card & Link Button Components

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('jobs', JobController::class)->only(['index']);
+Route::resource('jobs', JobController::class)->only(['index' ,'show']);
 
 Route::get('/', fn() => to_route('jobs.index'));
