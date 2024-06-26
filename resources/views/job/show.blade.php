4b8d9d4d18ca151745d930485b7059df5db83f7a
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-26 18:55:17 -0300

    Applying for Jobs: The Job Policy

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -6,6 +6,8 @@
 
     <x-job-card :job="$job">
         <p class="text-sm text-slate-500 mb-4">{!! nl2br(e($job->description)) !!}</p>
+
+        <x-link-button :href="route('job.application.create', ['job' => $job])">Apply</x-link-button>
     </x-job-card>
 
     <x-card class="mb-4">
