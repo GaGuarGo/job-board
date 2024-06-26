e0343ffc1d180b129e907e961d368e245bd1ea9b
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-26 16:43:35 -0300

    Authentication: Sign-in Form

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,7 +1,41 @@
 <x-layout>
 
-    <x-card>
-        Login!
+    <h1 class="my-16 text-center text-4xl font-medium text-slate-600">Sign in to your Account</h1>
+
+    <x-card class="py-8 px-16">
+        <form action="{{route('auth.store')}}" method="POST">
+            @csrf
+
+            <div class="mb-8">
+
+                <label for="email" class="mb-2 block text-sm font-medium text-slate-900">Email</label>
+                <x-text-input name="email"></x-text-input>
+
+            </div>
+            <div class="mb-8">
+
+                <label for="password" class="mb-2 block text-sm font-medium text-slate-900">Password</label>
+                <x-text-input name="password" type="password"></x-text-input>
+
+            </div>
+
+            <div class="mb-8 flex justify-between text-sm font-medium">
+                <div>
+                    <div class="flex items-center space-x-2">
+                        <input name="remember" type="checkbox" class="rounded-sm border border-slate-400"/>
+                        <label for="remember">Remember Me</label>
+                    </div>
+                </div>
+                <div>
+                    <a href="#" class="text-indigo-600 hover:underline">Forget Password?</a>
+                </div>
+            </div>
+
+            <x-button class="w-full bg-indigo-600 text-white hover:bg-indigo-700">
+                Login
+            </x-button>
+
+        </form>
     </x-card>
 
 </x-layout>
