930ad4784c05a07cd7599356a02131a02c590909
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-26 19:04:04 -0300

    Applying for Jobs: Did I Already Apply?

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -7,7 +7,11 @@
     <x-job-card :job="$job">
         <p class="text-sm text-slate-500 mb-4">{!! nl2br(e($job->description)) !!}</p>
 
-        <x-link-button :href="route('job.application.create', ['job' => $job])">Apply</x-link-button>
+        @can('apply', $job)
+            <x-link-button :href="route('job.application.create', ['job' => $job])">Apply</x-link-button>
+        @else
+        <div class="text-center text-sm font-medium text-slate-500"> You Already applied to this Job</div>
+        @endcan
     </x-job-card>
 
     <x-card class="mb-4">
