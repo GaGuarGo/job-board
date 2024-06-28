7aacb3039428422e2b737a3334f21c6d008077c9
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-28 13:01:56 -0300

    File Uploads: Uploading Files

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -11,7 +11,7 @@
             Your Job Application
         </h2>
 
-        <form action="{{route('job.application.store', $job)}}" method="POST">
+        <form action="{{route('job.application.store', $job)}}" method="POST" enctype="multipart/form-data">
             @csrf
 
             <div class="mb-4">
@@ -19,6 +19,11 @@
                 <label class="mb-2 block text-sm font-medium text-slate-900" for="expected_salary">Expected Salary</label>
                 <x-text-input type="number" name="expected_salary" />
 
+            </div>
+
+            <div class="mb-4">
+                <label for="mb-4 block text-sm font-medium text-slate-900">Upload CV</label>
+                <x-text-input type="file" name="cv"/>
             </div>
 
             <x-button class="w-full">Apply</x-button>
