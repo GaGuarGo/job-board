22c72ecd4e4797e6e83cbce841f39a3dfe231081
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-28 12:30:26 -0300

    My Applications: Applicant Count, Average Asking Salary

--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -4,7 +4,36 @@
 
     @forelse($applications as $application)
 
-        <x-job-card :job="$application->job"></x-job-card>
+        <x-job-card :job="$application->job">
+
+            <div class="flex justify-between items-center text-sm text-slate-500">
+
+                <div>
+                    <div>
+                        Applied {{$application->created_at->diffForHumans()}}
+                    </div>
+
+                    <div>
+                        Other {{Str::plural('applicant', $application->job->job_applications_count - 1)}}
+                        {{ $application->job->job_applications_count - 1}}
+                    </div>
+
+                    <div>
+                        Your asking salary ${{number_format($application->expected_salary)}}
+                    </div>
+                    <div>
+                        Average asking salary
+                        ${{number_format($application->job->job_applications_avg_expected_salary)}}
+                    </div>
+                </div>
+
+                <div>
+                    Right
+                </div>
+
+            </div>
+
+        </x-job-card>
 
     @empty
 
